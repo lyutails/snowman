490737299fc7f0fddf414f0dd10d9c4eefb14376
lyutails <lyusya.cool@gmail.com>
2024-01-15 19:49:38 +0300

    feat: put question in query

--- pick_random_question.js
+++ pick_random_question.js
@@ -12,54 +12,20 @@ function updateQueryString(value) {
 }
 
 export function pickRandomQuestion(questions) {
-  questionNumber = Math.floor(Math.random() * questions.length);
-  if (url.searchParams.get(`q${questionNumber}`) !== null) {
+  if (!window.location.search) {
     questionNumber = Math.floor(Math.random() * questions.length);
+  } else {
+    questionNumber = Math.floor(Math.random() * questions.length);
+    url.searchParams.get(`q${questionNumber}`) === `${questionNumber}` ?
+      (questionNumber = Math.floor(Math.random() * questions.length)) : questionNumber;
   }
   console.log(questionNumber);
 }
 
 function checkAnswerNumber(questions) {
-  if (!window.location.search) {
-    pickRandomQuestion(questions);
-    question.textContent = questions[questionNumber].question;
-    updateQueryString(questionNumber);
-  } else {
-    pickRandomQuestion(questions);
-    question.textContent = questions[questionNumber].question;
-    updateQueryString(questionNumber);
-
-    /* for (let i = 0; i < questions.length; i++) {
-        if (url.searchParams.get(`q${questions[i].id}`) === null) {
-          pickRandomQuestion(questions);
-          question.textContent = questions[questionNumber].question;
-          updateQueryString(questionNumber);
-          console.log(
-            questions[i].id,
-            url.searchParams.get(`q${questions[i].id}`),
-            `${questions[i].id}`
-          );
-        }
-      } */
-
-    /* questions.find((item) => {
-        if (url.searchParams.get(`q${item.id}`) !== `${item.id}`) {
-          console.log(
-            questions[item.id].id,
-            url.searchParams.get(`q${item.id}`),
-            `${item.id}`
-          );
-          pickRandomQuestion(questions);
-          question.textContent = questions[questionNumber].question;
-          updateQueryString(questionNumber);
-        }
-        if (url.searchParams.get(`q${item.id}`) === null) {
-          pickRandomQuestion(questions);
-          question.textContent = questions[questionNumber].question;
-          updateQueryString(questionNumber);
-        }
-      }); */
-  }
+  pickRandomQuestion(questions);
+  question.textContent = questions[questionNumber].question;
+  updateQueryString(questionNumber);
 }
 
 checkAnswerNumber(questions);
