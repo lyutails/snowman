090a31f1608301008394f13a4e553cbacbd694bf
lyutails <lyusya.cool@gmail.com>
2024-01-20 03:30:21 +0300

    fix: remove lalala console logs

--- pick_random_question.js
+++ pick_random_question.js
@@ -13,36 +13,23 @@ function updateQueryString(value) {
 export function pickRandomQuestion(questions) {
   if (!window.location.search) {
     let questionRandom = Math.floor(Math.random() * questions.length);
-    console.log("lalala");
-    console.log(+localStorage.getItem("que_lyu"), questionRandom);
     if (
       +localStorage.getItem("que_lyu") !== questionRandom ||
       !localStorage.getItem("que_lyu")
     ) {
       questionNumber = questionRandom;
-      console.log("lalala 1");
     }
     if (+localStorage.getItem("que_lyu") === questionRandom) {
-      console.log("lalala 3");
       pickRandomQuestion(questions);
     }
   } else {
     let questionRandom = Math.floor(Math.random() * questions.length);
-    console.log("lalala 4");
     const url = new URL(window.location);
-
-    console.log(
-      url.searchParams.get(`q${questionRandom}`),
-      `${questionRandom}`,
-      `q${questionRandom}`
-    );
     if (url.searchParams.get(`q${questionRandom}`) == null) {
       questionNumber = questionRandom;
-      console.log("lalala 5");
     }
     if (url.searchParams.get(`q${questionRandom}`)) {
       pickRandomQuestion(questions);
-      console.log("lalala 6");
     }
   }
 }
